03fa5a184575803b06098885cc60a5a3a5cd0b4a
KingWilliam-I <jrdmarriner8@gmail.com>
2020-04-24 21:16:57 -0400

    Still getting used to committing on terminal. Catching up to spaghett mining.

--- Main.py
+++ Main.py
@@ -3,12 +3,19 @@ from discord.ext import commands
 import discord.guild
 
 import json
+from datetime import datetime, timedelta, time, tzinfo
 from typing import Tuple, Dict, Any
+from Gambler import Gamble
+from level_stuff import *
+
 
 TOKEN = ' '
 client = commands.Bot(command_prefix='~')
 client.remove_command('help')
 data = dict()
+level_info = dict()
+identify = []
+casino = Gamble()
 
 
 def parse_payment(message):  # Returns payment amount from command
@@ -24,6 +31,11 @@ def ledger():
         json.dump(data, bankbook)
         bankbook.close()
 
+    with open("leveling.json", 'w') as leveling:
+        global level_info
+        json.dump(level_info, leveling)
+        leveling.close()
+
 
 @client.event
 async def on_ready():
@@ -31,7 +43,72 @@ async def on_ready():
         global data
         data = json.load(bankbook)
         bankbook.close()
+
+    with open("leveling.json") as leveling:
+        global level_info
+        level_info = json.load(leveling)
+        leveling.close()
+
+    with open("blacklist.txt", 'r') as ban:
+        global identify
+        identify = ban.readlines()
+        ban.close()
+
     print("Setup is complete!")
+
+
+@client.event
+async def on_message(message):
+    player = str(message.author.id)
+    words = message.content
+    timing = message.created_at
+    global identify
+
+    if player not in identify:
+        try:
+            level = level_info[player]['level']
+            last_time = datetime.strptime(level_info[player]['time'], '%X')
+        except:
+            level_info[player] = {"xp": 0.0, "level": 1, "time": timing.strftime("%X")}
+            level = 1
+            last_time = timing
+
+        difference = timing - last_time
+        if difference.seconds >= 60 and words.find("~") == -1:
+            level_info[player]['time'] = timing.strftime("%X")
+            cap = xp_cap(level)
+            xp = xp_yield(level)
+            level_info[player]['xp'] += float(xp)
+
+            if level_info[player]['xp'] > cap:
+                level_info[player]['xp'] -= float(cap)
+                level_info[player]['level'] += 1
+                data[player]['balance'] += 3
+                await message.channel.send("You've-a earned-a trio spaghett-a!")
+
+    ledger()
+    await client.process_commands(message)
+
+
+@client.command(pass_context=True)
+async def slots(ctx):
+    player = str(ctx.message.author.id)
+    content = ctx.message.split(" ")
+    amount = parse_payment(content)
+    data[player]['balance'] -= int(amount)
+
+    casino.setBet(amount, player)
+    winnings, end_msg, results = casino.roll_slots(player)
+    ctx.message.channel.send("You-a rolled and got-a {}", results)
+
+
+    if winnings > 0:
+        ctx.message.channel.send(end_msg)
+        data[player]['balance'] += int(winnings)
+    else:
+        ctx.message.channel.send(end_msg)
+
+    ledger()
 
 
 @client.command(pass_context=True)
@@ -50,7 +127,7 @@ async def stimulus(ctx):
     try:
         recipient = str(ctx.message.mentions[0].id)
     except:
-        print("Line 56 didn't work\n")
+        print("Line 89 didn't work\n")
 
     if recipient in data.keys():
         data[recipient]['balance'] += int(amount)
@@ -106,17 +183,19 @@ async def balance(ctx):
 
 
 @client.command(pass_context=True)
-@commands.is_owner()
+#@commands.is_owner()
 async def leave(ctx):
     ledger()
     await client.close()
     await client.logout()
+
 
 def get_Token():
     key = open("token.txt", "r")
     global TOKEN
     TOKEN = key.readline()
     key.close()
+
 
 get_Token()
 client.run(TOKEN)
@@ -127,16 +206,19 @@ client.run(TOKEN)
 #	[] Change your nickname
 #	[] Climb the ranks
 #       [] Betting/Gambling
-#		- lottery
-#		- rng (roulette, slots)
-#		- card games
-#		- Petty bets ("I bet 20 spaghett that billy is gonna fuck up as thatcher.")
+#		[] lottery
+#		[] rng (roulette, slots)
+#			[] output roll results
+#		[] card games
+#			[] Poker maybe
+#			[] Blackjack
+#		[] Petty bets ("I bet 20 spaghett that billy is gonna fuck up as thatcher.")
 #	[] Stonks
 #		HOW TO GET SPAGHOOTIES
 #	[] Text chat (like level/rank gaining in MEE6)
-#	[] Stimulus from admins (Money printer go brrrrrrrrrrrr)
-#	[] payment from others
-#	[] 
+#	[*] Stimulus from admins (Money printer go brrrrrrrrrrrr)
+#	[*] payment from others
+#	[] Gambling
 
-# TODO learn to interface with MEE6
+# TO DO learn to interface with MEE6
 #	is it even possible to interface? Does it have an api of some kind?
--- level_stuff.py
+++ level_stuff.py
@@ -4,5 +4,15 @@ import math
 
 def xp_cap(level):
 # Determine and return the amount of xp required to mine a spaghett
-    cap = log(3,level)+19.523
-    return cap
+#    cap = math.log(level,3)+59.523
+#    cap = 3*(math.sqrt(level))+level+50
+    cap = 3*(math.sqrt(level))+100
+    return float(cap)
+
+
+def xp_yield(level):
+    dividend1 = 3*(level**2)+10
+    dividend2 = (level**2)
+    xp = (dividend1/dividend2)+10
+
+    return float(xp)
